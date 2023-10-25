eb3337744b4a45b287d9765a399d895d51b596e0
VinuHegde17 <vinayhegde0820@gmail.com>
2023-10-25 09:37:44 +0530

    New

--- p27.java
+++ p27.java
@@ -3,8 +3,6 @@ public class p27 {
         System.out.println("Hello String");
     }
 public static void main(String args[]){
-    String sc= "String";
-    p27 p=new p27();
     int [] a =new int[4];
     int[] b= {2,4,6,8};
     a[0]=0;
@@ -13,6 +11,7 @@ public static void main(String args[]){
     a[3]=6;
     for(int i=0;i<a.length;i++){
         System.out.println("Value is "+a[i]);
+        System.out.println("Value is "+b);
     }
 }    
 }
